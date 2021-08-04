0cb1747afb97c11d554ce9603452ab56b278ee9f
Heliton Martins <helitonmrf@gmail.com>
2021-08-04 13:44:39 -0300

    :fire:  Moving tests to Core
    tests related to header/index detection belongs to core. Fixing that mistake.

--- GUI/tests/runtests.py
+++
@@ -1,9 +0,0 @@
-import sys, os
-sys.path.append(os.path.join(os.path.dirname(os.path.dirname(__file__))))
-
-import unittest
-
-from matrices_manipulations import *
-
-if __name__ == "__main__":
-    unittest.main()

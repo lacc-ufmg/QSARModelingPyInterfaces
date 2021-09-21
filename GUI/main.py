50503c86aff48e1bb319809623a58f76120db3f4
Heliton Martins <helitonmrf@gmail.com>
2021-09-20 21:47:57 -0300

    :art:  Moved logging config to a specific file.
    Now all logging-related config is grouped in logger.py, which provides an init() method that is called by main.py. It also provides an utility
    to silence matplotlib.

--- GUI/main.py
+++ GUI/main.py
@@ -1,6 +1,5 @@
 import sys
-import coloredlogs
-import logging
+import logger, logging
 from os import path
 from ValidationHandler import ValidationHandler
 from FilterHandler import FilterHandler
@@ -14,19 +13,8 @@ import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 
-from Constants import DEBUG_MODE
 
-
-logging_level = logging.DEBUG if DEBUG_MODE else logging.INFO
-if not DEBUG_MODE:
-    # redirect stderr and stdout to a temporary file
-    LOG_FILE = get_log_file()
-    print(f"Initialized log in {LOG_FILE}")
-    sys.stdout = open(LOG_FILE, 'a')
-    sys.stderr = sys.stdout
-
-coloredlogs.install(fmt="%(filename)s:%(lineno)s %(funcName)s() %(levelname)s  %(message)s", level=logging_level)
-coloredlogs.DEFAULT_FIELD_STYLES = {'filename': {'color': 'blue'}, 'lineno': {'color': 'blue'}, 'funcName': {'color': 'magenta'}, 'levelname': {'bold': True, 'color': 'black'}}
+logger.init()
 
 __DIR__ = getattr(sys, '_MEIPASS', path.abspath(path.dirname(__file__)))
 

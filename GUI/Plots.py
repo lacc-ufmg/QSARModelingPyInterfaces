c90c0517b728829594c87d6cbf94b7e8b776b90a
Heliton Martins <helitonmrf@gmail.com>
2021-09-27 20:29:37 -0300

    :sparkles:  Added plot options to GUI
    I added the plot selector to GUI, which is dinamically created.

--- GUI/Plots.py
+++ GUI/Plots.py
@@ -1,23 +1,28 @@
+from typing import Callable, Dict
 import numpy as np
 import logging, logger
 import matplotlib.pyplot as plt  # TODO: add do dependecies
 logger.silence_matplotlib_logger()
 from abc import ABC, abstractmethod
 from qsarmodelingpy.cross_validation_class import CrossValidation
+from qsarmodelingpy.yrandomization import YRandomization
 import Utils
-
 
 class Plots(ABC):
 
+    def __init__(self) -> None:
+        matplolib_config = Utils.read_config("matplotlib")
+        plt.rcParams.update(matplolib_config)
+
     @abstractmethod
-    def get_methods(self) -> dict:
+    def get_methods(self) -> Dict[str, Callable]:
         pass
 
 
 class CrossValidationPlots(Plots):
+
     def __init__(self) -> None:
-        matplolib_config = Utils.read_config("matplotlib")
-        plt.rcParams.update(matplolib_config)
+        super().__init__()
 
     def Q2_R2_evolution(self, cv: CrossValidation):
         xaxis = range(1, cv.nLVMax + 1)
@@ -56,11 +61,32 @@ class CrossValidationPlots(Plots):
         plt.ylabel("Predicted activity")
         plt.show()
 
-    def get_methods(self) -> list:
+    def get_methods(self) -> Dict[str, Callable]:
         return {
-            "Q² and R² × Latent Variables": self.Q2_R2_evolution,
+            "Q² and R² × Latent Variables (cross-validation)": self.Q2_R2_evolution,
             "Exprimental × Predicted (calibration)": self.y_versus_ŷ_cal,
             "Exprimental × Predicted (cross-validation)": self.y_versus_ŷ_CV,
+        }
+
+class YRandomizationPlots(Plots):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def Q2_versus_R2(self, yr: YRandomization):
+        R2_rand = yr.R2[:-1]
+        Q2_rand = yr.Q2[:-1]
+        R2_best = yr.R2[-1]
+        Q2_best = yr.Q2[-1]
+        plt.plot(R2_rand, Q2_rand, linestyle="None", marker='o', label="Randomized")
+        plt.plot(R2_best, Q2_best, linestyle="None", marker='o', label="Best model")
+        plt.title("y-randomization")
+        plt.xlabel("R²")
+        plt.ylabel("Q²")
+        plt.show()
+
+    def get_methods(self) -> Dict[str, Callable]:
+        return {
+            "Q² and R² × Latent Variables (y-randomization)": self.Q2_versus_R2,
         }
 
 
@@ -88,6 +114,10 @@ if __name__ == "__main__":
     methods = CrossValidationPlots().get_methods()
     logging.debug(f"{methods = }")
 
-    for method in methods:
-        methods[method](cv)
-        break
+    # for method in methods:
+    #     methods[method](cv)
+    #     break
+
+    yr = YRandomization(X,y,cv.nLVMax)
+    yrp = YRandomizationPlots()
+    yrp.Q2_versus_R2(yr)
--- GUI/ResultsHandler.py
+++ GUI/ResultsHandler.py
@@ -1,14 +1,16 @@
-import numpy as np
+from qsarmodelingpy.cross_validation_class import CrossValidation
+import os
+from typing import Dict, Callable
 import logging
-from Constants import DEBUG_MODE
+
+import pandas as pd
 from MainHandler import Handler
-# from qsarmodelingpy.Interfaces import ConfigExtValInterface
-# from runCalculations import RunCalculations
-# import os
 import gi
+
+from Plots import CrossValidationPlots, Plots
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
-import matplotlib.pyplot as plt # TODO: add do dependecies
+from gi.overrides import Gtk as Gtk3
 
 
 class ResultsHandler(Handler):
@@ -21,11 +23,62 @@ class ResultsHandler(Handler):
             'delete-event', lambda w, e: w.hide() or True)
         self.window.show_all()
 
+        self.plot_type_selector = self.builder.get_object(
+            'plot_selection_combo')
+
+        # TODO: remove this
+        self.show(self.get_object_for_testing_TODO(), CrossValidationPlots())
+
+    def show(self, dataobject, plotter: Plots):
+        self.dataobject = dataobject
+        self.plotter = plotter
+        self.init_selector(self.plotter.get_methods())
+        self.window.show_all()
+
+    def init_selector(self, methods: Dict[str, Callable]):
+        model = Gtk.ListStore(str)
+        for key in methods:
+            model.append([key])
+        logging.debug(model)
+        self.plot_type_selector.set_model(model)
+        renderer_text = Gtk.CellRendererText()
+        self.plot_type_selector.pack_start(renderer_text, True)
+        self.plot_type_selector.add_attribute(renderer_text, "text", 0)
+        self.plot_type_selector.set_active(0)
+
         # self.ext_val_config: ConfigExtValInterface
 
-    def on_plot(self, _) -> None:
-        x = np.random.rand(5)
-        y = np.random.rand(5)
-        plt.plot(x, y)
-        plt.show()
-        logging.info("yuhuu")
+    @staticmethod
+    def get_selected_method(elem: Gtk3.ComboBox) -> str:
+        tree_iter = elem.get_active_iter()
+        if tree_iter is not None:
+            model = elem.get_model()
+            name = model[tree_iter][0]
+            return name
+        else:
+            entry = elem.get_child()
+            return entry.get_text()
+
+    def on_plot(self, combobox: Gtk.ComboBox) -> None:
+        self.plotter.get_methods()[self.get_selected_method(
+            combobox)](self.dataobject)
+
+    # TODO: remove this
+    @staticmethod
+    def get_object_for_testing_TODO() -> CrossValidation:
+        """This is for TESTING ONLY and should be removed
+
+        Returns:
+            CrossValidation: The objetct to plot
+        """
+        directory = "/home/helitonmrf/Documents/QSAR/cancer_prostata/resultados"
+        X_matrix_file = "filtered_10_GA_X_sel.csv"
+        y_matrix_file = "../atividades.txt"
+
+        df = pd.read_csv(os.path.join(directory, X_matrix_file),
+                         sep=';', index_col=0)
+        X = df.to_numpy()
+        y = pd.read_csv(os.path.join(directory, y_matrix_file),
+                        sep=';', header=None).values
+        cv = CrossValidation(X, y)
+        return cv

296a3be87b01300811554629e6e090440cc3e9f9
Heliton Martins <helitonmrf@gmail.com>
2022-11-11 20:51:18 -0300

    :alien:  [CLI] Update runOPS2 to the new library

--- CLI/runOPS2.py
+++ CLI/runOPS2.py
@@ -2,18 +2,14 @@
 import pandas as pd
 from qsarmodelingpy.ops import OPS
 from qsarmodelingpy.cross_validation_class import CrossValidation
-from qsarmodelingpy.filter import variance_cut, correlation_cut, autocorrelation_cut
+from qsarmodelingpy.filter import filter_matrix #, variance_cut, correlation_cut, autocorrelation_cut
 from qsarmodelingpy import lj_cut as lj
 from qsarmodelingpy.validate_yr_lno import validate
+from qsarmodelingpy import Utils
 import os
 import argparse
 import logging
 import coloredlogs
-logging_level = logging.INFO
-coloredlogs.DEFAULT_FIELD_STYLES = {'filename': {'color': 'blue'}, 'lineno': {
-    'color': 'blue'}, 'funcName': {'color': 'magenta'}, 'levelname': {'bold': True, 'color': 'black'}}
-coloredlogs.install(
-    fmt="%(filename)s:%(lineno)s %(funcName)s() %(levelname)s  %(message)s", level=logging_level)
 
 
 
@@ -25,6 +21,10 @@ def run(filename, typeOPS):
         directory = dfConf[idx][0]
         xFile = dfConf[idx][1]
         yFile = dfConf[idx][2]
+
+        xpath = os.path.join(directory, xFile)
+        ypath = os.path.join(directory, yFile)
+
         var_cut = float(dfConf[idx][3])
         corr_cut = float(dfConf[idx][4])
         nLVOPS = None if dfConf.isnull()[1][5] else dfConf[idx][5]
@@ -39,35 +39,19 @@ def run(filename, typeOPS):
         out_matrix = dfConf[idx][14]
         out_cv = dfConf[idx][15]
         out_models = dfConf[idx][16]
-        df = pd.read_csv(os.path.join(directory, xFile), sep=';', index_col=0)
-        # Filtering the matrix according to the options in configuration file
-        dfX = lj.transform(df) if dfConf[idx][17].upper() == "YES" else df
-        logging.info("Dimensions of the original matrix")
-        logging.info(dfX.shape)
+        lj_transform = dfConf[idx][17].upper() == "YES"
         autoscale = dfConf[idx][18].upper() == "YES"
-        y = pd.read_csv(os.path.join(directory, yFile),
-                        sep=';', header=None).values
-        indVar = variance_cut(dfX.values, var_cut)
-        dfVar = dfX.loc[:, dfX.columns[indVar]]
-        logging.info("Dimensions of the matrix after variance cut")
-        logging.info(dfVar.shape)
-        indCorr = correlation_cut(dfVar.values, y, corr_cut)
-        dfCorr = dfVar.loc[:, dfVar.columns[indCorr]]
-        logging.info("Dimensions of the matrix after correlation cut")
-        logging.info(dfCorr.shape)
-        auto_cut = float(dfConf[idx][19])
-        indAuto = autocorrelation_cut(dfCorr.values, y, auto_cut)
-        dfRest = dfCorr.loc[:, dfCorr.columns[indAuto]]
-        logging.info("Dimensions of the matrix after auto correlation cut")
-        logging.info(dfRest.shape)
-        dfRest.to_csv(os.path.join(
-            out_directory, "filtered_" + out_matrix), sep=';')
-        X = dfRest.values
+        autocorr_cut = float(dfConf[idx][19])
+
+        df = Utils.load_matrix(xpath)
+        y = pd.read_csv(ypath, header=None).values
+        dfFiltered = filter_matrix(df, y, lj_transform, var_cut, corr_cut, autocorr_cut)
+        X = dfFiltered.values
         ops = OPS(X, y, nLVOPS, nLVModel, opsWindow,
                   opsIncrement, percentage, nModels, autoscale)
-        if typeOPS == 's':
+        if typeOPS.lower() == 's':
             ops.runOPS()
-        elif typeOPS == 'f':
+        elif typeOPS.lower() == 'f':
             ops.feedOPS()
         else:
             logging.error(
@@ -77,8 +61,8 @@ def run(filename, typeOPS):
         var_sel = validate(
             X, y, ops.models["var_sel"], ops.models["Q2"], yr_cut=yr_crit, lno_cut=lno_crit)
         if var_sel != []:
-            dfSel = dfCorr.loc[:, dfCorr.columns[var_sel]]
-            dfSel.to_csv(os.path.join(out_directory, out_matrix), sep=';')
+            dfSel = dfFiltered.loc[:, dfFiltered.columns[var_sel]]
+            dfSel.to_csv(os.path.join(out_directory, out_matrix))
             cv = CrossValidation(dfSel.values, y)
             cv.saveParameters(os.path.join(out_directory, out_cv))
         else:
@@ -95,8 +79,19 @@ if __name__ == '__main__':
                         metavar='<t>',
                         help='Type of OPS run (s for single and f for feed).'
                         )
+    parser.add_argument('--verbose', '-v', required=False, action='store_true', help="Print more detailed (and potentialy trash) information.")
 
     args = parser.parse_args()
     filename = args.filename
     typeOPS = args.type
+    verbose = args.verbose
+
+    logging_level = logging.DEBUG if verbose else logging.INFO
+    coloredlogs.DEFAULT_FIELD_STYLES = {'filename': {'color': 'blue'}, 'lineno': {
+        'color': 'blue'}, 'funcName': {'color': 'magenta'}, 'levelname': {'bold': True, 'color': 'black'}}
+    coloredlogs.install(
+        fmt="%(filename)s:%(lineno)s %(funcName)s() %(levelname)s  %(message)s", level=logging_level)
+    
+    logging.debug("Debug (verbose) mode active.")
+
     run(filename, typeOPS)
